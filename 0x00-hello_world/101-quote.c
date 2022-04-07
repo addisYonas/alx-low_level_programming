5de8f450b21a2499ad3eac06707a440446f2f7e5
addisYonas <addyonas@gmail.com>
2022-04-07 12:37:48 -0700

    UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-char output[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-size_t i=0; 
-size_t arrayLength = strlen( output );
-  for (  i = 0 ; i < arrayLength ; i++ )
-    {
-        putchar(output[i]);
-    }
+char output[] = "and that piece of art is useful\"-Dora Korpar,2015-10-19\n";
+size_t i = 0;
+size_t arrayLength = strlen(output);
+for (i = 0 ; i < arrayLength ; i++)
+{
+putchar(output[i]);
+}
 return (1);
 }
 
