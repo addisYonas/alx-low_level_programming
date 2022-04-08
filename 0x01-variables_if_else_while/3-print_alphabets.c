cc93862d43ac87c1214b25ccdb89cb2675476a28
addisYonas <addyonas@gmail.com>
2022-04-08 09:50:54 -0700

    lphabet in lowercase, and then in uppercase,

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,22 +8,23 @@
  */
 int main(void)
 {
-char ch[] = "abcdefghijklmnopqrstuvwxyz";
-int i = 0;
-int arrayLength = strlen(ch);
+	char ch[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	int arrayLength = strlen(ch);
 
-for (i = 0 ; i < arrayLength ; i++)
-{
-putchar(ch[i]);
-}
+	for (i = 0 ; i < arrayLength ; i++)
+		{
+		putchar(ch[i]);
+		}
 
-for (i = 0 ; i < arrayLength ; i++)
-{
-if (ch[i] >= 'a' && ch[i] <= 'z')
-{
-ch[i] = ch[i] - 32;
-putchar(ch[i]);
-}
-}
+	for (i = 0 ; i < arrayLength ; i++)
+	{
+	if (ch[i] >= 'a' && ch[i] <= 'z')
+	{
+		ch[i] = ch[i] - 32;
+		putchar(ch[i]);
+	}
+	}
+	putchar(' ');
 return (0);
 }
