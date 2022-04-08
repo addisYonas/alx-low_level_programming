05e280401880b8bcbc9a2578a97b4a1ccfd43e1d
addisYonas <addyonas@gmail.com>
2022-04-08 11:46:54 -0700

    combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description:prints all possible combinations of single-digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
