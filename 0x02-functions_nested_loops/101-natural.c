b74b9cd12c0f5b960da3ad40c7a5fce2e6480596
addisYonas <addyonas@gmail.com>
2022-04-12 12:42:57 -0700

    task 13 natural number below 10

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - main block
+* Description: computes and prints the sum of all the multiples of 3 or
+* 5 below 1024 (excluded), followed by a new line
+* Return: 0
+*/
+
+int main(void)
+{
+int i, sum;
+
+for (i = 0; i < 1024; i++)
+if (i % 3 == 0 || i % 5 == 0)
+sum += i;
+printf("%d\n", sum);
+return (0);
+}
