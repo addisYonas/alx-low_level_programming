0cb50597d85b2f48cd7ad7b1ef3eb496b68552a6
addisYonas <addyonas@gmail.com>
2022-04-14 03:36:48 -0700

    task 3 number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_number - print 0 to 9
+ *
+ * Return: Always 0.
+ */
+
+void print_number(void)
+{
+ int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i);
+		i++;
+	}
+		_putchar('\n');
+	}
