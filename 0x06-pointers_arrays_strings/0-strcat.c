e78a721cd00d9574d91da973929f22f33d5e6347
addisYonas <addyonas@gmail.com>
2022-04-20 12:40:27 -0700

    task 0 strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,10 +4,21 @@
  * *_strcat - concatenates two string
  * @dest:string
  * @src:string
- * Return: 0 or 1
+ * Return: dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	return (strcat(dest, src));
+	int i, a = -1;
+
+	for (i = 0; dest[i] != '\0'; i++);
+	
+	do
+	{
+		a++;
+		dest[i] = src[a];
+		i++;
+	} while (src[a] != '\0');
+
+	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcat - concatenates two string
+ * @dest:string
+ * @src:string
+ * @n:bytes from src
+ * Return: 0 or 1
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int length = strlen(src);
+
+	while (src[length)
+	return(strcat(dest, src));
+}
