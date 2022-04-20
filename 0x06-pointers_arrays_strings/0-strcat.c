2fcfd610dfc749b708fa364dac42fa369cac54eb
addisYonas <addyonas@gmail.com>
2022-04-20 11:14:49 -0700

    task 0 strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcat - concatenates two string
+ * @dest:string
+ * @src:string
+ * Return: 0 or 1
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
