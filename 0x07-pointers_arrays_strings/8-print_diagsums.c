0af8e768115500c3626ae093031ef6abcb5cfb11
addisYonas <addyonas@gmail.com>
2022-04-25 12:18:50 -0700

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
  * print_diagsums - description
  * @a: 2d array of int types
@@ -8,14 +8,14 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, sum = 0, sum1 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum += a[i];
-		sum1 += a[size - i];
+		sum1 += a[i];
+		sum2 += a[size - i - 1];
 		a += size;
 	}
-	printf("%d, ", sum);
-	printf("%d\n", sum1);
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
 }
