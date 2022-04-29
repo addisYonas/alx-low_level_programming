eebbac25ceebb4e1b459b6aea9193cda1089c7ee
addisYonas <addyonas@gmail.com>
2022-04-29 08:47:37 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into the program
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+	{
+	printf("%s\n",  argv[i]);
+	}
+}
