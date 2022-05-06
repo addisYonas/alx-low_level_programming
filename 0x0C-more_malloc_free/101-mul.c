fa723574ba6b2a18ff6d547987e5cb59b1d9f3f2
addisYonas <addyonas@gmail.com>
2022-05-06 12:32:28 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,20 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * _puts - prints a string, followed by a new line,
  * @str: pointer to the string to print
  * Return: void
 */
 
-
 void _puts(char *str)
 {
-int i = 0;
-while (str[i])
-{
+	int i = 0;
+
+	while (str[i])
+	{
 	_putchar(str[i]);
 	i++;
-}
+	}
 
 }
 
@@ -26,7 +27,8 @@ while (str[i])
 
 int _atoi(const char *s)
 {
-    int sign = 1;
+	int sign = 1;
+
 	unsigned long int resp = 0, firstNum, i;
 
 	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
@@ -55,16 +57,16 @@ int _atoi(const char *s)
 void print_int(unsigned long int n)
 {
 
-unsigned  long int divisor = 1, i, resp;
+	unsigned  long int divisor = 1, i, resp;
 
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+	;
 
-for (; divisor >= 1; n %= divisor, divisor /= 10)
-{
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
 	resp = n / divisor;
 	_putchar('0' + resp);
-}
+	}
 
 }
 
@@ -77,15 +79,15 @@ for (; divisor >= 1; n %= divisor, divisor /= 10)
 
 int main(int argc, char const *argv[])
 {
-(void)argc;
+	(void)argc;
 
 if (argc != 3)
-{
+	{
 	_puts("Error ");
 	exit(98);
-}
-print_int(_atoi(argv[1]) * _atoi(argv[2]));
-_putchar('\n');
+	}
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	_putchar('\n');
 
 return (0);
 }
