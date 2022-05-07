0689ce395df9efa712d975ddcdb91c261b5a76c1
addisYonas <addyonas@gmail.com>
2022-05-07 08:09:38 -0700

    task 5;

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,8 +11,8 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 unsigned int *initDigitArray(size_t size);
-void stringIntMultiply(unsigned int *prod_digits, char *n1_digits,
-		       char *n2_digits, size_t n1_len, size_t n2_len);
+void stringIntMultiply(unsigned int *prod_digits, char *n1_digits,char *n2_digits, size_t n1_len, size_t n2_len);
 int stringIsPosInt(char *s);
 void error(int status);
+
 #endif
