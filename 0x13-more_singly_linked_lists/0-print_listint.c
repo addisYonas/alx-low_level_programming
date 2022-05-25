925abe0befe3820fbc0a31613ee53c88c4119e5f
addis yonas <addyonas@gmail.com>
2022-05-25 12:12:56 -0700

    task 0 betty fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * print_listint - function that prints all the elements of a listint_t list
- *
- *
- * @h: list to print
- *
- * Return: The number of nodes.
- *
- */
+* print_listint - function that prints all the elements of a listint_t list
+*
+*
+* @h: list to print
+*
+* Return: The number of nodes.
+*
+*/
 
 size_t print_listint(const listint_t *h)
 {
