5d0631434a262e0ba69a92284dffb9bc97571e3b
addis yonas <addyonas@gmail.com>
2022-07-11 13:08:36 -0700

    task 2

---
+++ 0x18-dynamic_libraries/cal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+int add(int a, int b)
+{
+    int nAdd = a + b;
+    return nAdd;
+}
+int sub(int a, int b)
+{
+    int nsub = a -  b;
+    return nsub;
+}
+
+int mul(int a, int b)
+{
+    int nmul = a * b;
+    return nmul;
+}
+int div(int a, int b)
+{
+    int ndiv = a / b;
+    return ndiv;
+}
+int mod(int a, int b)
+{
+    int nmod = a % b;
+    return nmod;
+}
+
+
